93b07e1859dd70ef8920997eab50544ffa40e764
alphaalexey <homyakovost@gmail.com>
2025-09-18 10:26:21 +0700

    Lab2: initial commit

--- lab02/src/calculate.ts
+++ lab02/src/calculate.ts
@@ -1,5 +1,15 @@
-import { ReversePolishNotationActionDict} from "./rpn.ohm-bundle";
+import { ReversePolishNotationActionDict } from "./rpn.ohm-bundle";
 
 export const rpnCalc = {
-    // write rules here
+    Expr_num(n) {
+        return parseInt(n.sourceString, 10);
+    },
+
+    Expr_plus(a, b, _plus) {
+        return a.calculate() + b.calculate();
+    },
+
+    Expr_times(a, b, _star) {
+        return a.calculate() * b.calculate();
+    }
 } satisfies ReversePolishNotationActionDict<number>;
--- lab02/src/stackDepth.ts
+++ lab02/src/stackDepth.ts
@@ -1,5 +1,27 @@
 import { ReversePolishNotationActionDict } from "./rpn.ohm-bundle";
 
 export const rpnStackDepth = {
+    Expr_num(_n) {
+        const out = 1;
+        const max = 1;
+        return { max, out };
+    },
+
+    Expr_plus(a, b, _plus) {
+        const A = a.stackDepth;
+        const B = b.stackDepth;
+        const out = A.out + B.out - 1;
+        const max = Math.max(A.max, A.out + B.max);
+        return { max, out };
+    },
+
+    Expr_times(a, b, _star) {
+        const A = a.stackDepth;
+        const B = b.stackDepth;
+        const out = A.out + B.out - 1;
+        const max = Math.max(A.max, A.out + B.max);
+        return { max, out };
+    },
 } satisfies ReversePolishNotationActionDict<StackDepth>;
-export type StackDepth = {max: number, out: number};
+
+export type StackDepth = { max: number, out: number };
