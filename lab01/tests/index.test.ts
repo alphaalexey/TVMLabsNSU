108da617f0fce2e17f302b4115e587d50458228f
alphaalexey <homyakovost@gmail.com>
2025-09-18 10:20:44 +0700

    Lab1: format tests

--- lab01/tests/index.test.ts
+++ lab01/tests/index.test.ts
@@ -1,8 +1,7 @@
 import { evaluate, SyntaxError } from '../src';
 import { test } from '../../mark';
-  
-export function syncFail()
-{
+
+export function syncFail() {
     throw new Error("oops");
 }
 
@@ -20,12 +19,10 @@ describe('testing addition and multiplication', () => {
         35  +
         7`);
     test("Multiple additions are valid", 3, evaluate, 42, "5+4+3+2+1+2+3+4+5+6+7");
-    
     test("Multiplication is a valid expression", 3, evaluate, 42, "6*7");
     test("Incomplete multiplication is invalid", 3, evaluate, SyntaxError, "42*");
     test("Standalone * is invalid", 3, evaluate, SyntaxError, "*42");
     test("Multiplication is performed before addition", 4, evaluate, 42, "7+4*7+7");
-
     test("Complex expressions are supported", 4, evaluate, 42, "7+2*7+3*6+3");
     test("Parentheses are correctly supported", 4, evaluate, 42, "(2+1)*7+(3*2*3)+2*(1+0)+1");
     test("Extra paren are ok", 4, evaluate, 42, "(2+1)*7+(3*(2)*3)+2*((1+(0)))+1");
