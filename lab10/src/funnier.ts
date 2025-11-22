4d8bf96a855574835ea7c40e2bebe43c48307a90
alphaalexey <homyakovost@gmail.com>
2025-11-23 02:28:21 +0700

    Lab10: initial commit

--- lab10/src/funnier.ts
+++ lab10/src/funnier.ts
@@ -1,5 +1,19 @@
-import { Module } from 'lab08/src';
+import { Module, ParameterDef, Statement, Predicate } from 'lab08';
 
 
 export interface AnnotatedModule extends Module {
+    type: "module";
+    functions: AnnotatedFunctionDef[];
+}
+
+export interface AnnotatedFunctionDef {
+    type: "fun";
+    name: string;
+    parameters: ParameterDef[];
+    returns: ParameterDef[];
+    locals: ParameterDef[];
+    body: Statement;
+    
+    pre?: Predicate | null;
+    post?: Predicate | null;
 }
--- lab10/src/parser.ts
+++ lab10/src/parser.ts
@@ -2,11 +2,28 @@ import { MatchResult, Semantics } from 'ohm-js';
 
 import grammar, { FunnierActionDict } from './funnier.ohm-bundle';
 
-import { AnnotatedModule } from './funnier';
-import { parseFunny } from '../../lab08';
+import { AnnotatedModule, AnnotatedFunctionDef } from './funnier';
+import { ErrorCode, Predicate, checkModule, fail, getFunnyAst, parseOptional } from '../../lab08';
 
 const getFunnierAst = {
-    // write rules here
+    ...getFunnyAst,
+
+    PreSpec(_requires, pred) {
+        return pred.parse();
+    },
+
+    PostSpec(_ensures, pred) {
+        return pred.parse();
+    },
+
+    Function(name, _lp, params, _rp, preOpt, retSpec, postOpt, usesOpt, stmt) {
+        return {
+            ...getFunnyAst["Function"](name, _lp, params, _rp, preOpt, retSpec, postOpt, usesOpt, stmt),
+
+            pre: parseOptional<Predicate | null>(preOpt, null),
+            post: parseOptional<Predicate | null>(postOpt, null),
+        } as AnnotatedFunctionDef;
+    },
 } satisfies FunnierActionDict<any>;
 
 export const semantics: FunnySemanticsExt = grammar.Funnier.createSemantics() as FunnySemanticsExt;
@@ -20,5 +37,25 @@ interface FunnyActionsExt {
 }
 
 export function parseFunnier(source: string): AnnotatedModule {
-    return parseFunny(source);
+    const match: MatchResult = grammar.Funnier.match(source, "Module");
+
+    if (match.failed()) {
+        const m: any = match;
+        const pos =
+            typeof m.getRightmostFailurePosition === "function"
+                ? m.getRightmostFailurePosition()
+                : null;
+
+        const message: string =
+            m.message ?? "Syntax error in Funny module.";
+
+        fail(ErrorCode.ParseError, message, {
+            startLine: pos?.lineNum,
+            startCol: pos?.colNum,
+        });
+    }
+
+    const mod = (semantics as FunnySemanticsExt)(match).parse();
+    checkModule(mod);
+    return mod;
 }
